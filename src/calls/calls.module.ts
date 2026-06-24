07aac222e33f9ea1cb0dfc075fa4c3922fddbc6a
Yash-crpto <yashvendraparihar@gmail.com>
2026-06-24 11:07:15 +0530

    feat(calls): add calls module

---
+++ src/calls/calls.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { CallsController } from './calls.controller';
+import { CallsService } from './calls.service';
+import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({
+ imports:[PrismaModule],
+ controllers:[CallsController],
+ providers:[CallsService],
+})
+export class CallsModule {}
